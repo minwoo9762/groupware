446880eb7e18a87920be2db75051c56e56e99022
Naisaac05 <dltkr0505@gmail.com>
2025-08-18 14:46:49 +0900

    minwoo 3

--- src/main/java/com/himedia/groupware/controller/WorkController.java
+++ src/main/java/com/himedia/groupware/controller/WorkController.java
@@ -3,10 +3,14 @@ package com.himedia.groupware.controller;
 import com.himedia.groupware.dto.WorkBoardDto;
 import com.himedia.groupware.service.WorkBoardService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
@@ -41,4 +45,27 @@ public class WorkController {
 
      @GetMapping("/workBoardWrite")
     public String workBoardWrite(){return "work/workBoardWrite";}
+
+    @GetMapping("/updateBoard")
+    public String updateBoard(@RequestParam("dto") @Valid WorkBoardDto workboarddto , BindingResult result,
+                              Model model) {
+        String url ="work/updatdBoard";
+        WorkBoardDto bdto =bs.getBoardOne(workboarddto.getId());
+
+        if(result.hasFieldErrors("pass"))
+            model.addAttribute("msg", result.getFieldError("pass").getDefaultMessage());
+        else if (result.hasFieldErrors("title"))
+            model.addAttribute("msg", result.getFieldError("title").getDefaultMessage());
+        else if (result.hasFieldErrors("content"))
+            model.addAttribute("msg", result.getFieldError("content").getDefaultMessage());
+        else if(!workboarddto.getPass().equals(bdto.getPass()))
+            model.addAttribute("msg", "수정을 위한 패스워드가 일치하지 않습니다");
+        else{
+            url="redirect:/boardViewWithoutCnt?num="+workboarddto.getId();
+            bs.update(workboarddto);
+    }
+
+        return url;
+    }
+
 }
--- src/main/java/com/himedia/groupware/service/WorkBoardService.java
+++ src/main/java/com/himedia/groupware/service/WorkBoardService.java
@@ -22,4 +22,8 @@ public class WorkBoardService {
         System.out.println((list));
         return result;
     }
+
+    public WorkBoardDto getBoardOne(int id) {
+        return bdao.getboard(id);
+    }
 }
