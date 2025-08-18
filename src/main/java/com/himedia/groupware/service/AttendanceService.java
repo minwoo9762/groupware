92634d37813851314bb994231b444f0b3b6933e3
KimJeongJin <128561413+wjdwls8520@users.noreply.github.com>
2025-08-18 12:58:41 +0900

    kjj 테스트

--- src/main/java/com/himedia/groupware/service/AttendanceService.java
+++ src/main/java/com/himedia/groupware/service/AttendanceService.java
@@ -2,13 +2,14 @@ package com.himedia.groupware.service;
 
 import com.himedia.groupware.dao.AttendanceIDao;
 import com.himedia.groupware.dto.AttendanceDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.AliasFor;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AttendanceService {
 
-    @AliasFor
+    @Autowired
     AttendanceIDao atdidao;
 
     public void insertAttendance(AttendanceDto atddao) {
